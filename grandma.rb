834250f8486448dc4e5dbe41f4f8130a75d0cb94
Hiroki Kato <hkato@sas.upenn.edu>
2018-02-08 00:44:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(input)
-  if input.upcase == input
+  if input == input.upcase
     puts "NO, NOT SINCE 1938!"
   elsif input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
